f61ce42278bde2e51e81d6a5e7e121018a2ab79a
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-09-07 16:38:00 -0300

    add first controller

---
+++ restful-web-services/restful-web-services/src/main/java/com/gabrielgfw/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.gabrielgfw.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+// É necessário a anotação RestController para definir uma classe Controller. //
+@RestController
+public class HelloWorldController {
+	
+	// Definindo a Rota do tipo GET, assim como seu respectivo path. // 
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+	
+}
