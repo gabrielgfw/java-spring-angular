d7c1b88ff72693eb9c9a5963275e248551f05aa8
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-09-06 22:52:26 -0300

    new components and event bind test

--- angular-front-end/src/app/app.component.ts
+++ angular-front-end/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // Define a tag do componente.
   selector: 'app-root',
+  // Define o local do HTML do componente.
   templateUrl: './app.component.html',
+  // Define o estilo do componente.
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
