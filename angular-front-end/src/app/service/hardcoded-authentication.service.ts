8fc47a39be5bc065f6265edc7510d65b51469de8
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-09-07 14:12:42 -0300

    use sessionStorage to check if user is logged in

--- angular-front-end/src/app/service/hardcoded-authentication.service.ts
+++ angular-front-end/src/app/service/hardcoded-authentication.service.ts
@@ -8,9 +8,25 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password) {
+    console.log('Before: ' + this.isUserLoggedIn());
+
     if(username === "Gabriel" && password === "genius") {
+      sessionStorage.setItem('authenticatorUser', username)
+      console.log('After: ' + this.isUserLoggedIn());
       return true;
-    }
+
+    } else {
       return false;
+    }
+
   }
+
+  // Checa através do sessionStorage se existe valor armazenado.
+  // Se existir, significa que o login foi realizado.
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatorUser');
+    return !(user === null);
+
+  }
+
 }
