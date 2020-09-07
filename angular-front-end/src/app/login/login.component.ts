c6a84ce5d867facd6916548a76c27c60ee1faa22
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-09-07 00:43:21 -0300

    navigate to welcomePage when credencials are correct

--- angular-front-end/src/app/login/login.component.ts
+++ angular-front-end/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +13,15 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   handleLogin() {
     if(this.username === "Gabriel" && this.password === "genius") {
+      // Redireciona para o Welcome Page.
+      this.router.navigate(['welcome'])
       this.invalidLogin = false
     } else {
       this.invalidLogin = true
