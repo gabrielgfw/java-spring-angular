515affb29bce5009b3b35253fc99733792002738
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-09-07 03:16:27 -0300

    add service to authenticate user and password

--- angular-front-end/src/app/login/login.component.ts
+++ angular-front-end/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,13 +14,14 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit() {
   }
 
   handleLogin() {
-    if(this.username === "Gabriel" && this.password === "genius") {
+    if(this.hardcodedAuthenticationService.authenticate(this.username, this.password)) {
       // Redireciona para o Welcome Page.
       // Essa rota irá resgatar o atributo username e passar junto com a rota.
       this.router.navigate(['welcome', this.username])
