c8561fa0bbe78449ee93227f146b8816fd863b46
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-09-07 01:36:18 -0300

    ng directive on todo list html

--- angular-front-end/src/app/list-todos/list-todos.component.ts
+++ angular-front-end/src/app/list-todos/list-todos.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
 
+  todos = [
+    {
+      id : 1,
+      description : 'be a nice man'
+    },
+    {
+      id: 2,
+      description : 'learn guitar'
+    },
+    {
+      id : 3,
+      description : 'sleep well'
+    }
+  ]
+
   todo = {
     id : 1,
     description : 'be cool man'
